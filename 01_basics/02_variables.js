e1aeec03407f41516e7893b14336e9ff05e9f0b2
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-01-20 05:38:03 +0000

    Variables in javascript

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,15 @@
+const accountId=1234
+let accoutEmail="vishak@haka";
+var accountPass=12345
+accountcity="indore"
+  
+// accountId=2334 not allow
+accoutEmail="visha@ram";
+ accountPass=1111
+ accountcity="sundrel;"
+  
+console.table([accountcity,accountPass,accoutEmail,]);
+/*
+Prefer not use VAR 
+because of issues of in block scope and funtions scope;
+*/
