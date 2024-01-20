0f04a5bf66f38df0955493b8457080b620c920b0
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-01-20 10:57:35 +0000

    data types in js

---
+++ 01_basics/03_Datatypes.js
@@ -0,0 +1,14 @@
+// data types in java script
+let number = "vishal";
+let age = 4;
+/* 1- number
+   bigint 
+   null = standalone and it was type of a object
+   undefined = undefined type
+   boolean = ""
+   string
+   object
+   symbol = unique
+*/
+// console.log(typeof null);= type of object 
+ 
