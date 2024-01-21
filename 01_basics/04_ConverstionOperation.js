9046af5c2ba769ef2d7dc4c1c390a82202de7ad4
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-01-21 02:21:04 +0000

    Converstion of datatype

---
+++ 01_basics/04_ConverstionOperation.js
@@ -0,0 +1,10 @@
+let name = "vishal"
+let score = 34
+// console.log(typeofscore);
+// so we use
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+// note "33"=33
+// "33abc"=NaN
+// true =1/false = 0
