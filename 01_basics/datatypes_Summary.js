3789d93da7a759324c6ed359d8328df67e520bc8
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-01-23 17:36:23 +0000

    datatypes summaries

---
+++ 01_basics/datatypes_Summary.js
@@ -0,0 +1,26 @@
+// // there are two types of data types primtuve and non primitive
+// call by value
+/*
+string ,null ,undefined,boolean,number,bignumber,symbol
+REFRENCE TYPES NON PRIMITIVE
+(arrays,objects,functions)
+note\
+const outside temp=null;
+let usermail;//undefined\
+const id=symbol(1,2,3,4);
+const anotherId =symbol(12,3);
+console.log(Id===anotherId);
+const bignumber=123343456n;
+// NON PRIMITIVE
+const heros=("ram","lakshamn"),"rudra")
+object {
+    name:"vishal";
+    age:23;
+    functions(){
+        const myfunc=function()
+        {
+            console.log("hello word");
+        }
+    }
+}
+*/
