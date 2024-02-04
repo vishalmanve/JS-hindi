2b32ca94ae5ce00e76e2c1af6726a5bd64d05132
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-02-04 02:20:58 +0000

    math and nums

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,22 @@
+const score=499
+// console.log(score);
+const balance = new Number (200);
+// console.log(balance.toLocaleString());
+// console.log(balance.toString().length)
+const numbers=100000;
+// console.log(numbers.toLocaleString('en-IN'));
+
+// ***********Maths*********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.4));
+// console.log(Math.ceil(3.4));uper ki values ex o/p 4
+// console.log(Math.floor(3.4)); niche ki values jese o/p 3
+// console.log(Math.random()); ye hume 0 or 1 ke bich ki value btata he 
+// console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10)+1);
+// +1 isliye ki 0 ko avoide kre 
+const min =10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min +1))+min);
