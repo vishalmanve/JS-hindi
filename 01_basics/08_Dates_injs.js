dd467f4f2f667613a3b23918cadafd28a5860ee4
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-02-14 19:55:52 +0000

    dates in js

---
+++ 01_basics/08_Dates_injs.js
@@ -0,0 +1,12 @@
+//  dates 
+// let mydate = new Date();
+// console.table([mydate.toDateString(),mydate.toJSON(),mydate.toLocaleDateString(),mydate.toISOString()]);
+// let createmydate = new Date("01-01-2024")
+// let createmydate = new Date("2024-01-14")
+// console.log(createmydate.toLocaleString());
+
+// methods using dates 
+let newDate = new Date();
+// console.log(newDate.getDay());
+console.log(newDate.getSeconds());
+console.log(newDate.getFullYear());
