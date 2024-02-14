dafbe8bccc5085125caf4012cddcd183e0cf1529
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-02-14 21:16:49 +0000

    arrays in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,19 @@
+let array = [1,2,3,4,5];
+let mydon = ["vshal","nikit"]
+// console.log(mydon[1]);
+
+// methods in array 
+
+array.push(7)
+// array.pop()   apne aap delete krdeta he last elemt ko
+// console.log(array);
+// console.log(array.includes(9));
+let myarr= myarr.join();
+// console.log(myarr);
+// console.log(myarr);
+
+// slice and splice 
+console.log("A",myArr);
+const myn1=myArr.slice(1,3);
+console.log(myn1);
+console.log("B",myArr);
