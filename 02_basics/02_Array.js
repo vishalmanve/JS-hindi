b48a6934736c93f5ccece5d7067c940693d8741e
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-04-28 11:57:00 +0000

    basic aray

---
+++ 02_basics/02_Array.js
@@ -0,0 +1,9 @@
+const Sundrel=["sundar","raju","giliya"]
+const Rampura=["ravi","suresh","mangiya"]
+
+// Sundrel.push(Rampura)
+// console.log(Sundrel);
+const pedvi= Sundrel.concat(Rampura)
+// console.log(pedvi);
+const bhikron = [...Sundrel,...Rampura]
+console.log(bhikron);
