9580e0391f3148c18efa5dc5a01f5996ffd5d6b9
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-05-27 07:58:47 +0000

    basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// const  bala =  true
+// IF
+// const temp = 30
+// if (temp<41) {
+//     console.log("save life");
+// }else{
+//     console.log("not safe");
+
+// <,>,<=,>=,!=,==,===
+// scope 
+// const score = 200
+// if(score < 300)
+//     {
+//         const power =  "fly"
+//         console.log(`power is ${power}`);
+//     }
+// imlisit type 
+
+// const balance= 1000;
+// if(balance<1500) console.log("yes");
+// if(balance <500){
+//     console.log("less than 500");
+// }
+//  else if(balance <750){
+//     console.log("less than 750");
+// }
+//  else if(balance <900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1400");
+// }
+// multiple conditions 
+
+const userloggedIn = true
+const debidcard = true
+const money = true
+if(userloggedIn && debidcard && money){
+    console.log("all this cheked");
+}
