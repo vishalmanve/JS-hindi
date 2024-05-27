a124895b1ec69a12924b84e16dee731de45d8a92
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-05-27 19:07:57 +0000

    about for loop

---
+++ 05_iteration/forLoop.js
@@ -0,0 +1,40 @@
+// FOR LOOP
+
+for(let bala = 0;bala<=10;bala++)
+    {
+        const num = bala;
+        if(num == 5){
+            // console.log("5 is the number ");
+        }
+        // console.log("the number is ",num);
+    }
+
+
+    for (let i = 0; i <=10; i++) {
+       for (let j = 0; j <=10; j++) {
+    //    console.log(i+ "*" + j +"=" + i*j)
+        
+       }
+        
+    }
+
+
+    let heros =["ram","hanumanji","bala"]
+    for (let bala = 0; bala < heros.length; bala++) {
+        // const element = heros[bala];
+        // console.log(element);
+        
+    }
+
+    // BREAK AND CONTINEOUS OPERATOR
+
+    for(let i = 1;i<=10;i++)
+        {
+            if(i==5){
+                // console.log(`5 is good `);
+            
+            break;
+            }
+            // console.log(`value of i is ${i}`);
+        }
+    
