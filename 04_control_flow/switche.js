5f6bf87add796025c5b55559ced1157c56a634a4
vishalmanve <104748708+vishalmanve@users.noreply.github.com>
2024-05-28 07:46:51 +0000

    switches

---
+++ 04_control_flow/switche.js
@@ -0,0 +1,19 @@
+// }
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+
+    default:
+        console.log("degfault ");
+        break;
+}
